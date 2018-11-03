e66c2628e0d47d5056106fe9e2112ad701d1b513
Jason <jtiino00@gmail.com>
2018-11-02 20:13:43 -0700

    Added bullet test

---
+++ Boss-Shooter/Assets/Scripts/BossFiring.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFiring : MonoBehaviour
+{
+
+    public GameObject bulletprefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fired");
+            GameObject Clone;
+
+            Clone = (Instantiate(bulletprefab, transform.position, transform.rotation)) as GameObject;
+
+            Destroy(Clone, 1);
+
+        }
+    }
+}
---
+++ Boss-Shooter/Assets/Scripts/BulletBasic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBasic : MonoBehaviour {
+
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(new Vector2(0, (speed*Time.deltaTime)));
+
+	}
+}
