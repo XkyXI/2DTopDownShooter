7d1ab30c2a8deea01f83f4c2310c18d52f264615
Jerome Benasfre <arkserionb@yahoo.com>
2018-11-02 20:17:23 -0700

    Created 2 health bars for 2 bosses.

--- Boss-Shooter/Assets/Scripts/enemyBehavior.cs
+++ Boss-Shooter/Assets/Scripts/enemyBehavior.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemyBehavior : MonoBehaviour {
 	[Header("Enemy Health")]
 	public float startingHealth = 1f; //the max health of enemy gameObject
 	public float currentHealth; //current health of the enemy gameObject
+    public Image healthBar;
+    public Text healthText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		//initalize the current health to starting health
-		currentHealth = startingHealth; 
-        
-	}
+		currentHealth = startingHealth;
+        healthText.text = (currentHealth / startingHealth * 100).ToString() + "%";
+        healthBar.fillAmount = currentHealth / startingHealth;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		//if current health is 0, this object should die
-		if (currentHealth <= 0)
+        //if current health is 0, this object should die
+        healthText.text = (currentHealth).ToString() + "%";
+        healthBar.fillAmount = currentHealth;
+        if (currentHealth <= 0)
 		{
-			Die();
+            healthText.text = 0.ToString() + "%";
+            Die();
 
 		}
         Debug.Log(currentHealth);
